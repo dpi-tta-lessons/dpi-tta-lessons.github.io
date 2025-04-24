b7cbbd1aec4b90c2b52859bccbe8b7a318e3f4cf
Ian Heraty <heratyian@gmail.com>
2025-04-24 11:34:08 -0500

    Fix bug with system dark mode

--- assets/js/controllers/theme_controller.js
+++ assets/js/controllers/theme_controller.js
@@ -36,7 +36,11 @@ export default class extends Controller {
 
   applyTheme(theme) {
     if (theme === "auto") {
-      document.documentElement.removeAttribute("data-bs-theme");
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.setAttribute("data-bs-theme", "dark");
+      } else {
+        document.documentElement.setAttribute("data-bs-theme", "light");
+      }
     } else {
       document.documentElement.setAttribute("data-bs-theme", theme);
     }

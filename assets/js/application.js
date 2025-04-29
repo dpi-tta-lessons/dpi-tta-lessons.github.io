18e866a396eae97c79fba0a9d7f2d671ca751eae
Ian Heraty <heratyian@gmail.com>
2025-04-29 16:19:19 -0500

    Upgrade theme picker to button toggle

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,12 +1,12 @@
 import { Application } from "@hotwired/stimulus";
 import BootController from "controllers/boot_controller";
 import LiveCodeController from "controllers/live_code_controller";
-import ThemeController from "controllers/theme_controller";
+import ThemeToggleController from "controllers/theme_toggle_controller";
 import TocController from "controllers/toc_controller";
 
 window.Stimulus = Application.start();
 
 Stimulus.register("boot", BootController);
 Stimulus.register("live-code", LiveCodeController);
-Stimulus.register("theme", ThemeController);
+Stimulus.register("theme-toggle", ThemeToggleController);
 Stimulus.register("toc", TocController);
